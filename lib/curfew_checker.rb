fa5ede88d365bbc668f4d5d9a24a35d2a3c25b06
KarinaMealey <kem32002@gmail.com>
2018-06-19 19:17:13 +0000

    automatically backed up by learn

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -10,9 +10,6 @@ simple_curfew_checker(1)
 
 
 
-def curfew_checker(time)
-  # code goes here
-end
 
 def complex_curfew_checker(time)
   if time<10
@@ -29,9 +26,15 @@ end
 complex_curfew_checker(11)
 
 
+
+
 def deluxe_curfew_checker(time)
   # code goes here
 end
+
+
+
+
 
 def platinum_curfew_checker(current_time, curfew_time)
   # code goes here
